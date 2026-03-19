ea4e8c08dabdc09e21f6c7e7f0b9d3a9bfced367
Oliver Nagy <naolipth@gmail.com>
2026-03-19 18:03:55 +0100

    [MOD] Add control flow for repo cloning

--- installer.sh
+++ installer.sh
@@ -8,6 +8,14 @@ echo "Running system update..."
 sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 
 REPO_URL="https://github.com/naolipth/linux-setup.git"
-INSTALL_DIR="$HOME/Projects"
+INSTALL_DIR="$HOME/Projects/linux-setup"
 
+echo "Cloning setup repository.."
+
+if [ -d "$INSTALL_DIR" ]; then
+  echo "Repository exists. Pulling latest changes"
+  git -C "$INSTALL_DIR" pull
+else
+  git clone "$REPO_URL" "$INSTALL_DIR"
+fi
 
