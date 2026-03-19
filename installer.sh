feae79ab9fe99b46968df74cd6fcba9df6550287
Oliver Nagy <naolipth@gmail.com>
2026-03-19 17:36:27 +0100

    [MOD] Added missing apt-get for autoremove

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@
 set -xeuo pipefail
 
 echo "Running system update..."
-sudo apt-get update -y && sudo apt-get upgrade -y && sudo autoremove -y
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 
 REPO_URL="https://github.com/naolipth/linux-setup.git"
 INSTALL_DIR="$HOME/Projects"
