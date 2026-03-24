d2fd674da90143669282d25301b1ec581480d643
Oliver Nagy <naolipth@gmail.com>
2026-03-24 21:16:49 +0100

    [TMP] Commented out autoupdate, for faster iteration on VMs

--- installer.sh
+++ installer.sh
@@ -4,8 +4,8 @@
 
 set -euo pipefail
 
-echo "Running system update..."
-sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y
+#echo "Running system update..."
+#sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 
 REPO_URL="https://github.com/naolipth/linux-setup.git"
 INSTALL_DIR="$HOME/Projects/linux-setup"
